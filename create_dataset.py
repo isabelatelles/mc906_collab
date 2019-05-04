daf5d226020e641071bfa0da346ed5175628f753
Isabela Telles <isabela.telles@gitlab.com>
2019-05-04 20:46:09 -0300

    update formula that calculates the number of trilpets and twins

--- create_dataset.py
+++ create_dataset.py
@@ -1,22 +1,23 @@
 from pandas import DataFrame
 from numpy import arange, random
+import math
 
 n_children = 200
-n_triplets = int(0.015 * n_children)
-n_twins = int(0.04 * n_children)
+triplets = math.ceil(0.015 * n_children / 3.) * 3
+twins = math.ceil(0.04 * n_children / 2.) * 2
 
-n_gift_types = 40
-n_gift_unities = 3
+n_gift_type = 40
+n_gift_quantity = 3
 
-wishlist_size = int(0.2 * n_gift_types)
-goodkids_size = int(0.1 * n_children)
+n_gift_pref = int(0.2 * n_gift_type)
+n_child_pref = int(0.1 * n_children)
 
-child_wishlist = random.randint(low=0, high=n_gift_types, size=(n_children, wishlist_size))
+child_wishlist = random.randint(low=0, high=n_gift_type, size=(n_children, n_gift_pref))
 df_child_wishlist = DataFrame(data=child_wishlist, index=arange(n_children))
 df_child_wishlist.to_csv(index=False, header=False, path_or_buf="created_child_wishlist_"
-                            + str(n_children) + "_" + str(n_gift_types) + ".csv")
+                            + str(n_children) + "_" + str(n_gift_type) + ".csv")
 
-gift_goodkids = random.randint(low=0, high=n_children, size=(n_gift_types, goodkids_size))
-df_gift_goodkids = DataFrame(data=gift_goodkids, index=arange(n_gift_types))
+gift_goodkids = random.randint(low=0, high=n_children, size=(n_gift_type, n_child_pref))
+df_gift_goodkids = DataFrame(data=gift_goodkids, index=arange(n_gift_type))
 df_gift_goodkids.to_csv(index=False, header=False, path_or_buf="created_gift_goodkids_"
-                            + str(n_children) + "_" + str(n_gift_types) + ".csv")
+                            + str(n_children) + "_" + str(n_gift_type) + ".csv")
