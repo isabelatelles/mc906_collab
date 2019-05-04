171b6256939ca4e8e2c26a445b6921b76dc24d4b
nathyharumi <nathalia.kuromiya@gmail.com>
2019-05-04 16:33:28 -0300

    save the created dataset

--- create_dataset.py
+++ create_dataset.py
@@ -13,6 +13,10 @@ goodkids_size = int(0.1 * n_children)
 
 child_wishlist = random.randint(low=0, high=n_gift_types, size=(n_children, wishlist_size))
 df_child_whishlist = DataFrame(data=child_wishlist, index=arange(n_children))
+df_child_whishlist.to_csv(index=False, header=False, path_or_buf="created_child_whishlist_"
+                            + str(n_children) + "_" + str(n_gift_types) + ".csv")
 
 gift_goodkids = random.randint(low=0, high=n_children, size=(n_gift_types, goodkids_size))
 df_gift_goodkids = DataFrame(data=gift_goodkids, index=arange(n_gift_types))
+df_gift_goodkids.to_csv(index=False, header=False, path_or_buf="created_gift_goodkids_"
+                            + str(n_children) + "_" + str(n_gift_types) + ".csv")
