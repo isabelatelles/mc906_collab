a406de72d663ad807ac0b506b1c35268cd9f1efc
Isabela Telles <isabela.telles@gitlab.com>
2019-05-04 15:56:52 -0300

    add create dataset

---
+++ create_dataset.py
@@ -0,0 +1,18 @@
+from pandas import DataFrame
+from numpy import arange, random
+
+n_children = 200
+n_triplets = int(0.015 * n_children)
+n_twins = int(0.04 * n_children)
+
+n_gift_types = 40
+n_gift_unities = 3
+
+wishlist_size = int(0.2 * n_gift_types)
+goodkids_size = int(0.1 * n_children)
+
+child_wishlist = random.randint(low=0, high=n_gift_types, size=(n_children, wishlist_size))
+df_child_whishlist = DataFrame(data=child_wishlist, index=arange(n_children))
+
+gift_goodkids = random.randint(low=0, high=n_children, size=(n_gift_types, goodkids_size))
+df_gift_goodkids = DataFrame(data=gift_goodkids, index=arange(n_gift_types))
