9f727aab96b6694c0733e73663a5b56ad3e8fe91
Isabela Telles <isabela.telles@gitlab.com>
2019-05-05 02:24:28 -0300

    modify avg normalized happines to receive a problem and an individual - a list of gifts given to the children

--- fitness_function.py
+++ fitness_function.py
@@ -7,28 +7,6 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import math
 from collections import Counter
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-from subprocess import check_output
-
-print(check_output(["ls", "dataset"]).decode("utf8"))
-
-n_children = 200  # n children to give
-n_gift_type = 40  # n types of gifts available
-n_gift_quantity = 3  # each type of gifts are limited to this quantity
-n_gift_pref = 100  # number of gifts a child ranks
-n_child_pref = 1000  # number of children a gift ranks
-twins = math.ceil(0.04 * n_children / 2.) * 2  # 4% of all population, rounded to the closest number
-triplets = math.ceil(0.015 * n_children / 3.) * 3  # 1.5% of all population, rounded to the closest number
-ratio_gift_happiness = 2
-ratio_child_happiness = 2
-
-gift_pref = pd.read_csv('dataset/created_child_wishlist_' + str(n_children) + '_' + str(n_gift_type) + '.csv',
-                        header=None).drop(0, 1).values
-child_pref = pd.read_csv('dataset/created_gift_goodkids_' + str(n_children) + '_' + str(n_gift_type) + '.csv',
-                         header=None).drop(0, 1).values
-
 
 def lcm(a, b):
     """Compute the lowest common multiple of a and b"""
@@ -36,46 +14,51 @@ def lcm(a, b):
     return a * b // math.gcd(a, b)
 
 
-def avg_normalized_happiness(pred, child_pref, gift_pref):
-    # check if number of each gift exceeds n_gift_quantity
-    gift_counts = Counter(elem[1] for elem in pred)
+def avg_normalized_happiness(problem, individual):
+    ratio_gift_happiness = 2
+    ratio_child_happiness = 2
+
+    gift_pref = pd.read_csv('dataset/created_child_wishlist_' + str(problem.n_children) + '_' +
+                            str(problem.n_gift_types) + '.csv', header=None).drop(0, 1).values
+    child_pref = pd.read_csv('dataset/created_gift_goodkids_' + str(problem.n_children) + '_' +
+                             str(problem.n_gift_types) + '.csv', header=None).drop(0, 1).values
+
+    # check if number of each gift exceeds problem.n_gift_quantity
+    gift_counts = Counter(elem for elem in individual)
     for count in gift_counts.values():
-        assert count <= n_gift_quantity
+        assert count <= problem.n_gift_quantity
 
     # check if triplets have the same gift
-    for t1 in np.arange(0, triplets, 3):
-        triplet1 = pred[t1]
-        triplet2 = pred[t1 + 1]
-        triplet3 = pred[t1 + 2]
+    for t1 in np.arange(0, problem.n_triplets, 3):
+        triplet1 = individual[t1]
+        triplet2 = individual[t1 + 1]
+        triplet3 = individual[t1 + 2]
         # print(t1, triplet1, triplet2, triplet3)
-        assert triplet1[1] == triplet2[1] and triplet2[1] == triplet3[1]
+        assert triplet1 == triplet2 and triplet2 == triplet3
 
     # check if twins have the same gift
-    for t1 in np.arange(triplets, triplets + twins, 2):
-        twin1 = pred[t1]
-        twin2 = pred[t1 + 1]
+    for t1 in np.arange(problem.n_triplets, problem.n_triplets + problem.n_twins, 2):
+        twin1 = individual[t1]
+        twin2 = individual[t1 + 1]
         # print(t1)
-        assert twin1[1] == twin2[1]
+        assert twin1 == twin2
 
-    max_child_happiness = n_gift_pref * ratio_child_happiness
-    max_gift_happiness = n_child_pref * ratio_gift_happiness
+    max_child_happiness = problem.n_gift_pref * ratio_child_happiness
+    max_gift_happiness = problem.n_child_pref * ratio_gift_happiness
     total_child_happiness = 0
-    total_gift_happiness = np.zeros(n_gift_type)
+    total_gift_happiness = np.zeros(problem.n_gift_types)
 
-    for row in pred:
-        child_id = row[0]
-        gift_id = row[1]
-
+    for child_id, gift_id in enumerate(individual):
         # check if child_id and gift_id exist
-        assert child_id < n_children
-        assert gift_id < n_gift_type
+        assert child_id < problem.n_children
+        assert gift_id < problem.n_gift_types
         assert child_id >= 0
         assert gift_id >= 0
-        child_happiness = (n_gift_pref - np.where(gift_pref[child_id] == gift_id)[0]) * ratio_child_happiness
+        child_happiness = (problem.n_gift_pref - np.where(gift_pref[child_id] == gift_id)[0]) * ratio_child_happiness
         if not child_happiness:
             child_happiness = -1
 
-        gift_happiness = (n_child_pref - np.where(child_pref[gift_id] == child_id)[0]) * ratio_gift_happiness
+        gift_happiness = (problem.n_child_pref - np.where(child_pref[gift_id] == child_id)[0]) * ratio_gift_happiness
         if not gift_happiness:
             gift_happiness = -1
 
@@ -83,23 +66,20 @@ def avg_normalized_happiness(pred, child_pref, gift_pref):
         total_gift_happiness[gift_id] += gift_happiness
 
     print('normalized child happiness=',
-          float(total_child_happiness) / (float(n_children) * float(max_child_happiness)), \
-          ', normalized gift happiness', np.mean(total_gift_happiness) / float(max_gift_happiness * n_gift_quantity))
+          float(total_child_happiness) / (float(problem.n_children) * float(max_child_happiness)),
+          ', normalized gift happiness',
+          np.mean(total_gift_happiness) / float(max_gift_happiness * problem.n_gift_quantity))
 
     # to avoid float rounding error
     # find common denominator
     # NOTE: I used this code to experiment different parameters, so it was necessary to get the multiplier
     # Note: You should hard-code the multipler to speed up, now that the parameters are finalized
-    denominator1 = n_children * max_child_happiness
-    denominator2 = n_gift_quantity * max_gift_happiness * n_gift_type
+    denominator1 = problem.n_children * max_child_happiness
+    denominator2 = problem.n_gift_quantity * max_gift_happiness * problem.n_gift_types
     common_denom = lcm(denominator1, denominator2)
     multiplier = common_denom / denominator1
 
     # # usually denom1 > demon2
     return float(math.pow(total_child_happiness * multiplier, 3) + math.pow(np.sum(total_gift_happiness), 3)) / float(
         math.pow(common_denom, 3))
-    # return math.pow(float(total_child_happiness)/(float(n_children)*float(max_child_happiness)),2) + math.pow(np.mean(total_gift_happiness) / float(max_gift_happiness*n_gift_quantity),2)
-
-
-random_sub = pd.read_csv('').values.tolist()
-print(avg_normalized_happiness(random_sub, child_pref, gift_pref))
+    # return math.pow(float(total_child_happiness)/(float(n_children)*float(max_child_happiness)),2) + math.pow(np.mean(total_gift_happiness) / float(max_gift_happiness*problem.n_gift_quantity),2)
--- modeling.py
+++ modeling.py
@@ -1,21 +1,24 @@
 import math
+from fitness_function import avg_normalized_happiness
 
 class SantaProblem():
     def __init__(self, n_children, n_gift_types):
-        self.n_children = n_children
-        self.n_gift_types = n_gift_types
-        self.n_triplets = math.ceil(0.015 * n_children / 3.) * 3
-        self.n_twins = math.ceil(0.04 * n_children / 2.) * 2
-        self.n_gifts = int(n_children/n_gift_types)
+        self.n_children = n_children  # n children to give
+        self.n_gift_types = n_gift_types  # n types of gifts available
+        self.n_gift_quantity = int(n_children/n_gift_types)  # each type of gifts are limited to this quantity
+        self.n_gift_pref = int(0.2 * n_gift_types)  # number of gifts a child ranks
+        self.n_child_pref = int(0.1 * n_children)  # number of children a gift ranks
+        self.n_triplets = math.ceil(0.015 * n_children / 3.) * 3  # 4% of all population, rounded to the closest number
+        self.n_twins = math.ceil(0.04 * n_children / 2.) * 2  # 1.5% of all population, rounded to the closest number
 
     def __repr__(self):
         return "Santa Gifting Problem: {} types of presents for {} children".format(self.n_gift_types, self.n_children)
 
-    def crossover(individual_1, individual_2):
+    def crossover(self, individual_1, individual_2):
         return individual_1
 
-    def mutation(individual):
+    def mutation(self, individual):
         return individual
 
-    def fitness_function(individual):
-        return 1
+    def fitness_function(self, individual):
+        return avg_normalized_happiness(self, individual)
