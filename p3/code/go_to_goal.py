17113831f1290ffe9593292f7faa1b6c1a04f44a
nathyharumi <nathalia.kuromiya@gmail.com>
2019-06-11 01:59:54 -0300

    add some comments

--- p3/code/go_to_goal.py
+++ p3/code/go_to_goal.py
@@ -2,8 +2,6 @@ import numpy as np
 from math import pi, atan2
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-
-import math
 import time
 import sys
 sys.path.insert(0, '../src')
@@ -14,16 +12,18 @@ class go_to_goal():
     def __init__(self, goal):
         self.goal = goal
         self.NUM_SENSORS = 8
-        self.max_speed = math.pi
+        self.max_speed = pi
         self.steps = 0.01
 
     def init_fuzzy(self):
+        # Set the distances to avoid obstacles
         distance = []
         for i in range(0, self.NUM_SENSORS):
             distance.append(ctrl.Antecedent(np.arange(0, 5.0, self.steps), 's' + str(i)))
             distance[i]['close'] = fuzz.trapmf(distance[i].universe, [0.0, 0.0, 0.3, 0.6])
             distance[i]['away'] = fuzz.trapmf(distance[i].universe, [0.3, 0.6, 5.0, 5.0])
 
+        # Set the direction to align the robot to its goal
         direction = ctrl.Antecedent(np.arange(-pi, pi, 0.0001), "direction")
         direction['left'] = fuzz.trimf(direction.universe, [-pi, -pi/2, 0])
         direction['right'] = fuzz.trimf(direction.universe, [0, pi/2, pi])
@@ -37,22 +37,29 @@ class go_to_goal():
         v_right['positive'] = fuzz.trimf(v_left.universe, [0.0, self.max_speed, self.max_speed])
         v_right['negative'] = fuzz.trimf(v_left.universe, [-self.max_speed + 0.1, -self.max_speed + 0.1, 0.0])
 
+        # Group sensor reading by its positions
         far_from_obstacles = distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] & distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away']
         obstacle_on_right = distance[4]['close'] | distance[5]['close'] | distance[6]['close'] | distance[7]['close']
         obstable_on_left = distance[0]['close'] | distance[1]['close'] | distance[2]['close'] | distance[3]['close']
         obstacle_in_front = distance[2]['close'] | distance[3]['close'] | distance[4]['close'] | distance[5]['close']
 
+        #Rules for left wheel
+            #Rules to avoid obstacles and don't get stuck due to confliting rules (obstacle + alignment)
         rule_l1 = ctrl.Rule(obstable_on_left, v_left['positive'])
         rule_l2 = ctrl.Rule(obstacle_on_right & obstacle_in_front, v_left['negative'])
         rule_l3 = ctrl.Rule(obstacle_on_right & ~ obstacle_in_front, v_left['positive'])
+            # Rules to align the robot to its goal and walk towards it
         rule_l4 = ctrl.Rule(far_from_obstacles & (direction['front'] | direction['right']), v_left['positive'])
         rule_l5 = ctrl.Rule(far_from_obstacles & direction['left'], v_left['negative'])
         #rule_l3 = ctrl.Rule(direction['front'] | direction['right'], v_left['positive'])
         #rule_l4 = ctrl.Rule(direction['left'], v_left['negative'])
 
+        #Rules for right wheel
+            #Rules to avoid obstacles and don't get stuck due to confliting rules (obstacle + alignment)
         rule_r1 = ctrl.Rule(obstable_on_left & obstacle_in_front, v_right['negative'])
         rule_r2 = ctrl.Rule(obstable_on_left & ~ obstacle_in_front, v_right['positive'])
         rule_r3 = ctrl.Rule(obstacle_on_right, v_right['positive'])
+            # Rules to align the robot to its goal and walk towards it
         rule_r4 = ctrl.Rule(far_from_obstacles & (direction['front'] | direction['left']), v_right['positive'])
         rule_r5 = ctrl.Rule(far_from_obstacles & direction['right'], v_right['negative'])
         #rule_r3 = ctrl.Rule(direction['front'] | direction['left'], v_right['positive'])
@@ -72,9 +79,11 @@ class go_to_goal():
             return result - 2*pi
 
     def get_vel(self, dist, pos, orient):
+        # Calculate the angle between the goal and the robot's current position
         a = atan2(pos[1] - self.goal[1], self.goal[0] - pos[0])
+        # Get the angle the robot needs to rotate
         angle = self._sum_angles(a, orient[2])
-        print("Angle: ", angle)
+        #print("Angle: ", angle)
         self.fuzzy_system.input["direction"] = angle
 
         for i in range(len(dist)):
@@ -84,6 +93,7 @@ class go_to_goal():
         return [self.fuzzy_system.output['vl'], self.fuzzy_system.output['vr']]
 
     def goal_test(self, position):
+        # Compares the robot position and the goal, assuming a 0.05 threshold
         return position[0] > self.goal[0] - 0.05 \
             and position[0] < self.goal[0] + 0.05 \
             and position[1] > self.goal[1] - 0.05 \
@@ -107,6 +117,8 @@ def main():
         robot.set_right_velocity(vel[1])
         time.sleep(0.2)
 
+    # Make sure the robot stop when get to the goal - This for was added due to
+    # some failures at sensor writing
     for i in range(4):
         robot.set_left_velocity(0)
         robot.set_right_velocity(0)
