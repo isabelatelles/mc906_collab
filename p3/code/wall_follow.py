728ef923af2cd485141172bf9ec844576932caab
Isabela Telles <isabela.telles@gitlab.com>
2019-06-12 02:04:37 -0300

    add comments

--- p3/code/wall_follow.py
+++ p3/code/wall_follow.py
@@ -24,20 +24,19 @@ class WallFollow:
         Make fuzzy setup by defining membership functions and rules
         """
 
-        # Set the difference between the right frontal sensor and the right back sensor
+        # Set the difference between the right front sensor and the right back sensor
         difference_right_sensors = ctrl.Antecedent(np.arange(-1.0, 1.0, 0.001), 'difference_right_sensors')
         difference_right_sensors['negative'] = fuzz.trimf(difference_right_sensors.universe, [-1.0, -1.0, 0.0])
         difference_right_sensors['zero'] = fuzz.trimf(difference_right_sensors.universe, [-0.015, 0.0, 0.015])
         difference_right_sensors['positive'] = fuzz.trimf(difference_right_sensors.universe, [0.0, 1.0, 1.0])
 
+        # Set the distances of the front sensors
         distances_front_sensors = list()
         for i in range(3):
             distances_front_sensors.append(ctrl.Antecedent(np.arange(0, 5.0, self.steps),
                                                            'distance_front_sensor_' + str(i + 1)))
             distances_front_sensors[i]['close'] = fuzz.trapmf(distances_front_sensors[i].universe, [0.0, 0.0, 0.3, 0.8])
             distances_front_sensors[i]['away'] = fuzz.trapmf(distances_front_sensors[i].universe, [0.3, 0.8, 5.0, 5.0])
-
-        distances_front_sensors[0].view()
 
         # Define available velocities for each wheel
         v_left = ctrl.Consequent(np.arange(-self.max_speed, 1.0 + self.max_speed, 0.01), 'vl')
